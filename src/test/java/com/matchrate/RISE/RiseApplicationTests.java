53a8d3d065a2b1172f51778cf513b4276fbc7331
nzeh daniel <nzehakujobi@gmail.com>
2024-04-24 13:24:01 +0100

    First commit

---
+++ src/test/java/com/matchrate/RISE/RiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matchrate.RISE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
